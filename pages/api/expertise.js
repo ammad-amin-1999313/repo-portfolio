39ba8da885aec08a36b3f2a807eba644c8020e7a
ammad amin <153178783+ammad-amin-1999313@users.noreply.github.com>
2024-10-03 23:31:33 +0500

    initial

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -1,31 +1,32 @@
 const expertise = [
     {
-        id: 0,
-        title: 'Full Stack Development',
-        desc: 'I am a skilled MERN stack developer with extensive experience in building robust web applications using MongoDB, Express.js, React.js, and Node.js. I possess a strong understanding of server-side rendering, API integration, and database management. With my expertise, I can deliver dynamic and efficient web solutions tailored to meet the unique requirements of clients.',
+      id: 0,
+      title: 'Full Stack Development',
+      desc: 'Experienced MERN stack developer with expertise in building web applications using MongoDB, Express, React, and Node.js. Proficient in server-side rendering, API integration, and database management, delivering efficient and tailored solutions.',
     },
     {
-        id: 1,
-        title: 'Scrum and Jira',
-        desc: "As a developer, I'm proficient in Jira and Scrum methodologies. I use Jira to simplify project management tasks like task assignment, prioritization, and progress tracking. Scrum has improved my teamwork, utilizing daily stand-ups, sprint planning, and retrospectives. My expertise ensures high-quality software products are delivered promptly.",
+      id: 1,
+      title: 'Figma Conversion',
+      desc: 'Skilled in converting Figma designs into fully functional and responsive web applications. Expertise in translating design concepts into pixel-perfect code, ensuring consistency and smooth user experience.',
     },
     {
-        id: 2,
-        title: 'Designing (UI/UX)',
-        desc: 'I am proficient in using design software such as Figma, Adobe XD, and Adobe Illustrator. Through participating in hackathons and freelance work, I have gained practical experience in UI/UX design and graphic design. Designing is my passion, and it has allowed me to continue to develop my creative skills and grow in the field.',
+      id: 2,
+      title: 'Docker',
+      desc: 'Proficient in using Docker for containerization, streamlining development workflows, and ensuring consistent environments across development and production. Experienced in building, deploying, and managing applications with Docker.',
     },
     {
-        id: 3,
-        title: 'Github',
-        desc: "GitHub is essential for version control and collaboration. Its intuitive interface and features like pull requests and issue tracking simplify the process. Continuous integration ensures up-to-date code. GitHub is vital to my success as a developer.",
+      id: 3,
+      title: 'GitHub',
+      desc: 'Proficient in GitHub for version control and collaboration. Expertise in pull requests, issue tracking, and continuous integration for streamlined development.',
     },
     {
-        id: 4,
-        title: 'Open Source Contributor',
-        desc: "Open Source is the future. I usually take some time on weekend and contribute into opensource project. It gives me opportunity to learn from best developer's practices and also gives me a chance to help others and contribute into the community for the good.",
+      id: 4,
+      title: 'Open Source Contributor',
+      desc: 'Regularly contribute to open-source projects, learning best practices and giving back to the developer community on weekends.',
     },
-]
-
-export default function handler(req, res) {
-    res.status(200).json(expertise)
-}
+  ];
+  
+  export default function handler(req, res) {
+    res.status(200).json(expertise);
+  }
+  
--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -2,37 +2,46 @@
 const recommendationCard = [
   {
     id: 0,
-    name: 'Shahmir Minhas',
-    image: "images/shahmir.jfif",
-    designation: 'React JS | NEXT JS | Node | GraphQL',
-    view: "Hey everyone! I wanted to take a moment to recommend Osama Javaid for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness to learn and grow, and his enthusiasm for tackling new challenges was contagious. Osama is a reliable and dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟",
-    linkednURL: "https://www.linkedin.com/in/shahmir-minhas/"
+    name: 'Shaheer Shaid',
+    designation: 'React JS | NEXT JS | Node | TypeScript',
+    view: `Hey everyone! I wanted to take a moment to recommend Ammad Amin for any ReactJs opportunities. 
+           I had the pleasure of working with him on multiple projects where he showcased a strong understanding 
+           of React.js and Next.js concepts and delivered high-quality code. He consistently demonstrated a willingness 
+           to learn and grow, and his enthusiasm for tackling new challenges was contagious. Ammad is a reliable and 
+           dedicated team member who would be a great asset to any React.js team. Highly recommended! 👍! 🌟`,
+    linkednURL: 'https://www.linkedin.com/in/shaheer-shahid03/'
   },
   {
     id: 1,
-    name: 'Sikandar Hayat',
-    image: "images/sikandar.jpeg",
-    designation: 'Data Scientist | Machine Learning Engineer | Python Developer',
-    view: "I had the privilege of working alongside Osama on multiple university projects, and I must say, his talent and work ethic truly stood out. Recently, we were both part of the dynamic team at JMM Technologies, where Osama's professionalism and attention to detail shone brightly. I wholeheartedly endorse Osama for any opportunity that calls for a highly skilled and dedicated individual.",
-    linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/"
-  },
+    name: 'Shahbaz Khan',
+    designation: 'Front-End Developer | System Integration Specialist',
+    view: `I had the privilege of working alongside Ammad Amin on multiple university projects, where his talent and work ethic truly stood out. 
+           We later collaborated at JMM Technologies, where Ammad's professionalism and attention to detail were exceptional. 
+           His expertise in frontend development and his ability to seamlessly integrate complex systems make him a valuable asset. 
+           I wholeheartedly endorse Ammad for any opportunity that requires a highly skilled and dedicated individual.`,
+    linkednURL: 'https://www.linkedin.com/in/shahbaz-khan-229408133/'
+}
+,
   {
     id: 2,
-    name: 'Nasir Khan',
-    image: "images/nasir.jpg",
-    designation: 'MERN | JavaScript | Tailwind | Sass | Bootstrap',
-    view: "I wholeheartedly recommend Osama as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. Osama's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable addition to any development team.",
-    linkednURL: "https://www.linkedin.com/in/nasirkhan22/"
+    name: 'Talha Majeed',
+    designation: 'MERN Stack Developer',
+    view: `I wholeheartedly recommend Ammad Amin as a talented React frontend developer with an incredible flair for UI/UX design. 
+           His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications. 
+           Ammad's attention to detail, problem-solving skills, and dedication to delivering exceptional results make him a valuable 
+           addition to any development team.`,
+    linkednURL: 'https://www.linkedin.com/in/talha-majeed-6464471a0/'
   },
   {
     id: 3,
-    name: 'Muhammad Ullah',
-    image: "images/muhammad.jpeg",
-    designation: 'MERN Stack Developer at Productbox',
-    view: "I highly recommend Osama Javed for web frontend development positions. Their expertise in ReactJS and Next.js, combined with their professionalism and dedication, make them an invaluable asset to any team.",
-    linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/"
+    name: 'Muhammad Fahad',
+    designation: 'MERN | JavaScript | Tailwind | MUI | Bootstrap',
+    view: `I highly recommend Ammad Amin for web frontend development positions. His expertise in ReactJS and Next.js, combined 
+           with his professionalism and dedication, make him an invaluable asset to any team.`,
+    linkednURL: 'https://www.linkedin.com/in/muhammadullahafridi/'
   },
-]
+];
+
 export default function handler(req, res) {
-  res.status(200).json(recommendationCard)
+  res.status(200).json(recommendationCard);
 }
